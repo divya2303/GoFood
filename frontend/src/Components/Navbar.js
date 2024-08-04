21d6ab9db1ebf4b0e2f5cc47380606c2f75ad314
divya2303 <114051737+divya2303@users.noreply.github.com>
2024-08-04 10:35:56 +0530

    Update Navbar.js

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
   const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("authToken");
-    navigate("/");
+    navigate("/login");
   }
   return (
     <div>
